c19d60fdcf95efe86a8e84fa9bd99235888a08da
Kushal Neupane <mkushal10@gmail.com>
2020-07-14 11:59:01 +0545

    first commit

---
+++ d1.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number: "))
+for row in range(1,num+1):
+    for col in range(1,num+1):
+        print("*",end='')
+    print()
---
+++ design_square.py
@@ -0,0 +1,4 @@
+for row in range(1,6):
+    print()
+    for col in range(1,6):
+        print("*",end='')
